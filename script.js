f3a8ecfc7aef836e2da22e9efddfd32675203f5d
Muhammad Wajahat Ali <wajahat6759@gmail.com>
2025-10-01 00:55:24 +0500

    script.js

---
+++ script.js
@@ -0,0 +1,7 @@
+// You can add more complex animations or interactivity here.
+// For example, an alert that pops up after 5 seconds:
+/*
+setTimeout(() => {
+    alert("Have a great day!");
+}, 5000);
+*/
